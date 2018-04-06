da3571c31dbf668b422770f8f38263456e9059bf
Kamila2908 <dudkiewicz.kamila@yahoo.com>
2018-04-06 19:47:59 +0200

    Completed task

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function drawTree (treeHeight) {
+    for ( var i = 0 ; i < 5 ; i++) {
+        var star = '';
+        for ( var j = 0 ; j < i ; j = i) {
+        var star += '*';
+       };
+       console.log(star);
+    };
+}
