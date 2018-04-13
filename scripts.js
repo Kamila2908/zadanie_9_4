d23173d4995d49fab1147cf9afd53474a69cb30b
Kamila2908 <dudkiewicz.kamila@yahoo.com>
2018-04-13 19:43:33 +0200

    Update js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 function drawTree (treeHeight) {
     for ( var i = 0 ; i < 5 ; i++) {
         var star = '';
-        for ( var j = 0 ; j = i ; j++) {
+        for ( var j = 0 ; j < i ; j++) {
         var star += "*";
        };
        console.log(star);
     };
 }
+drawTree(3)
